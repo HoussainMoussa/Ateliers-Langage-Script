a9c326b7aeaa78a1519b07c6aa5583930ab14a9a
HoussainMoussa <134783678+HoussainMoussa@users.noreply.github.com>
2026-05-27 12:09:16 +0200

    Create Atelier4.py

---
+++ Systèmes/Pathlib/Atelier4.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+
+def decomposer (chemin: str) -> tuple[str, str, str]:
+
+    p = Path(chemin)
+    nom = p.stem
+    extension = p.suffix
+    dossier = str(p.parent)
+    return (dossier, nom, extension)
+
+def afficher(chemin):
+
+    dossier, nom, extension = decomposer (chemin)
+    print(f"{chemin:<25} -> ('{dossier}', '{nom}', '{extension}')")
+
+def main():
+
+    exemples = [
+        "/tmp/a.txt",
+        "/var/log/archive.tar.gz",
+        "/etc/hosts",
+    ]
+
+    for chemin in exemples:
+        afficher(chemin)
+
+if __name__ == "__main__":
+    main()
+  
+  #Résultats de l'éxécution
+
+#/tmp/a.txt                -> ('/tmp', 'a', '.txt')
+#/var/log/archive.tar.gz   -> ('/var/log', 'archive.tar', '.gz')
+#/etc/hosts                -> ('/etc', 'hosts', '')
